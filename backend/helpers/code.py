5896b760140889b7fa711230a7067214fd3a672f
ksofiadiazmontiel <888716@stud.unive.it>
2023-11-30 16:13:31 +0100

    Conteggio NA values

--- backend/helpers/code.py
+++ backend/helpers/code.py
@@ -1,4 +1,5 @@
 import csv 
+import pandas as pd
 
 dove_alloggiare = '/app/app/dove-alloggiare.csv'
 with open(dove_alloggiare, "r", newline= "", encoding="utf-8") as file_csv:
@@ -7,3 +8,19 @@ with open(dove_alloggiare, "r", newline= "", encoding="utf-8") as file_csv:
 
     for riga in lettore_csv:
         print(riga)
+
+
+# Creazione di un DataFrame 
+df = pd.read_csv('/app/app/dove-alloggiare.csv')
+
+# Controllo dei valori mancanti in tutto il DataFrame
+valori_mancanti_totali = df.isnull().sum().sum()
+
+if valori_mancanti_totali > 0:
+    print(f"È presente un totale di {valori_mancanti_totali} valori mancanti nel DataFrame.")
+    # Mostra il conteggio dei valori mancanti per ogni colonna
+    valori_mancanti_per_colonna = df.isnull().sum()
+    print("Conteggio dei valori mancanti per ogni colonna:")
+    print(valori_mancanti_per_colonna)
+else:
+    print("Non ci sono valori mancanti nel DataFrame.")
