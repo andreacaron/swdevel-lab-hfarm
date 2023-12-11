9c8d0e6e568025d88b379b485ba3e583f0e4cd95
andreacaron <150248809+andreacaron@users.noreply.github.com>
2023-12-11 17:13:18 +0100

    Creation of frontend function and html page

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -7,7 +7,8 @@ This module defines a simple Flask application that serves as the frontend for t
 from flask import Flask, render_template
 import requests  # Import the requests library to make HTTP requests
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
@@ -20,6 +21,12 @@ BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 class QueryForm(FlaskForm):
     person_name = StringField('Person Name:')
     submit = SubmitField('Get Birthday from FastAPI Backend')
+
+class SearchForm(FlaskForm):
+    piscina_coperta = SelectField('Indoor Pool:',  choices=[('Vero', 'Si'), ('Falso', 'No')])
+    sauna = SelectField('Sauna:', choices=[('Vero', 'Si'), ('Falso', 'No')])
+    area_fitness = SelectField('Fitness area:',  choices=[('Vero', 'Si'), ('Falso', 'No')])
+    submit = SubmitField('Search')
 
 
 @app.route('/')
@@ -78,6 +85,31 @@ def internal():
             error_message = f'Error: Unable to fetch birthday for {person_name} from FastAPI Backend'
 
     return render_template('internal.html', form=form, result=None, error_message=error_message)
+
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_structure():
+    form = SearchForm()
+    error_message = None 
+
+    if form.validate_on_submit():
+        sauna = form.sauna.data
+        piscina = form.piscina_coperta.data
+        fitness = form.area_fitness.data
+        fastapi_url = f'{FASTAPI_BACKEND_HOST}/cerca_strutture?piscina_coperta={piscina}&sauna={sauna}&area_fitness={fitness}'
+        try:
+            response = requests.get(fastapi_url)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+            data_from_fastapi, error_message = response.json(), None
+        except requests.exceptions.RequestException as e:
+            data_from_fastapi, error_message = None, f'Error: {str(e)}'
+
+        return render_template("search.html", form=form, result=data_from_fastapi, error_message=error_message)
+
+    return render_template('search.html', form=form, result=None, error_message=error_message)
+
+
 
 
 if __name__ == '__main__':
