43e009eb313843926639109698fc53c1fefed287
ksofiadiazmontiel <888716@stud.unive.it>
2023-12-14 22:44:01 +0100

    Frontend

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,84 +1,75 @@
-"""
-Frontend module for the Flask application.
-
-This module defines a simple Flask application that serves as the frontend for the project.
-"""
-
-from flask import Flask, render_template
-import requests  # Import the requests library to make HTTP requests
+# Importing necessary modules
+from flask import Flask, render_template, request
+import requests
+from wtforms import StringField, SubmitField, SelectField
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+import pandas as pd
 
+# Creating a Flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+app.config['SECRET_KEY'] = 'your_secret_key' # Setting the secret key for CSRF protection
 
-# Configuration for the FastAPI backend URL
-FASTAPI_BACKEND_HOST = 'http://backend'  # Replace with the actual URL of your FastAPI backend
+# Defining the FastAPI backend host
+FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 
+# Defining the search form using Flask-WTF
+class SearchForm(FlaskForm):
+    # Create SelectFields for user input
+    aria_condizionata = SelectField('Air Conditioning:', choices=[('Vero', 'Yes'), ('Falso', 'No')])
+    animali_ammessi = SelectField('Pets Allowed:', choices=[('Vero', 'Yes'), ('Falso', 'No')])
+    submit = SubmitField('Search')
 
-class QueryForm(FlaskForm):
-    person_name = StringField('Person Name:')
-    submit = SubmitField('Get Birthday from FastAPI Backend')
+# Defining the route for search functionality
+@app.route('/search', methods=['GET', 'POST'])
+def search_structure():
+    # Create a form instance
+    form = SearchForm(request.form)
+    error_message = None
+    result = None
 
-
-@app.route('/')
-def index():
-    """
-    Render the index page.
-
-    Returns:
-        str: Rendered HTML content for the index page.
-    """
-    # Fetch the date from the backend
-    date_from_backend = fetch_date_from_backend()
-    return render_template('index.html', date_from_backend=date_from_backend)
-
-def fetch_date_from_backend():
-    """
-    Function to fetch the current date from the backend.
-
-    Returns:
-        str: Current date in ISO format.
-    """
-    backend_url = 'http://backend/get-date'  # Adjust the URL based on your backend configuration
-    try:
-        response = requests.get(backend_url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        return response.json().get('date', 'Date not available')
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching date from backend: {e}")
-        return 'Date not available'
-
-
-@app.route('/internal', methods=['GET', 'POST'])
-def internal():
-    """
-    Render the internal page.
-
-    Returns:
-        str: Rendered HTML content for the index page.
-    """
-    form = QueryForm()
-    error_message = None  # Initialize error message
-
+    # Handling form submission
     if form.validate_on_submit():
-        person_name = form.person_name.data
+        aria_condizionata = form.aria_condizionata.data
+        animali_ammessi = form.animali_ammessi.data
 
-        # Make a GET request to the FastAPI backend
-        fastapi_url = f'{FASTAPI_BACKEND_HOST}/query/{person_name}'
+        # Making a request to the FastAPI backend with user input
+        fastapi_url = f'{FASTAPI_BACKEND_HOST}/essential_services_periphery?aria_condizionata={aria_condizionata}&animali_ammessi={animali_ammessi}'
+        
+        # Requesting data from the FastAPI backend
         response = requests.get(fastapi_url)
 
+        # Process response from the FastAPI backend
         if response.status_code == 200:
-            # Extract and display the result from the FastAPI backend
+             # Extracting and displaying the result from the FastAPI backend
             data = response.json()
-            result = data.get('birthday', f'Error: Birthday not available for {person_name}')
-            return render_template('internal.html', form=form, result=result, error_message=error_message)
+            result = data  # Assigning the retrieved data to 'result'
+            return render_template('search.html', form=form, result=result, error_message=error_message)
         else:
-            error_message = f'Error: Unable to fetch birthday for {person_name} from FastAPI Backend'
+            error_message = f'Error'
 
-    return render_template('internal.html', form=form, result=None, error_message=error_message)
+    # Rendering the search.html template with form and error_message
+    return render_template('search.html', form=form, result=None, error_message=error_message)
 
+# Defining another route to display mock search results
+@app.route('/display_results', methods=['GET'])
+def display_results():
+    # Mock search results data (can be replaced with real data)
+    result = [
+        {
+            'DENOMINAZIONE': 'Hotel 1',
+            'PROVINCIA': 'Province 1',
+            'COMUNE': 'City 1',
+            'INDIRIZZO': 'Address 1',
+            'NUMERO CIVICO': '123',
+            'CAP': '12345',
+            'EMAIL': 'hotel1@example.com'
+        },
+    ]
 
+    # Rendering the results.html template with result data
+    return render_template('results.html', result=result)  
+
+# Starting the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
