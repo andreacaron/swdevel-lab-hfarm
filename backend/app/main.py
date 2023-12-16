557c88dd3f96e06f0bf2090b5940049e277315c4
Satourr <samirasatour01@gmail.com>
2023-12-16 23:52:04 +0100

    Final revision

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,36 +1,48 @@
+# Import FastAPI framework for building APIs quickly
 from fastapi import FastAPI
+# Import JSONResponse for returning custom JSON responses
 from fastapi.responses import JSONResponse
+# Import pandas for data manipulation and analysis
 import pandas as pd
+# Import uvicorn for running the FastAPI application
 import uvicorn
 
+# Create an instance of the FastAPI application
 app = FastAPI()
 
+# Read CSV data into a pandas DataFrame
 df_suburb = pd.read_csv('/app/app/dove-alloggiare.csv')
 
-# Handling NA values
+''' Handle missing values (NA) by replacing them
+with empty strings in the DataFrame '''
 df_suburb.fillna('', inplace=True)
 
-@app.get("/find_structures_suburb")
-def find_structures_suburb(Typology,English,French,German,Spanish):
 
-    # Extract relevant data for the frontend
-    conditions = (df_suburb['TIPOLOGIA'] == Typology) & \
-        (df_suburb['INGLESE'] == English) & \
-        (df_suburb['FRANCESE'] == French) & \
-        (df_suburb['TEDESCO'] == German) & \
-        (df_suburb['SPAGNOLO'] == Spanish)& \
+@app.get("/find_structures_suburb")
+# Extract relevant data for the frontend based on specified conditions
+def find_structures_suburb(Typology, English, French, German, Spanish):
+    conditions = (
+        (df_suburb['TIPOLOGIA'] == Typology) &
+        (df_suburb['INGLESE'] == English) &
+        (df_suburb['FRANCESE'] == French) &
+        (df_suburb['TEDESCO'] == German) &
+        (df_suburb['SPAGNOLO'] == Spanish) &
         (df_suburb['PERIFERIA'] == 'Vero')
-    
+    )
+# Filter DataFrame based on conditions and convert
+# the results to a list of dictionaries
     results = df_suburb[conditions].to_dict(orient='records')
     return results
 
+
 @app.get("/get_typology")
 def get_typology():
+    #  Extract unique values of 'TIPOLOGIA' column from DataFrame
     df_tipologie = df_suburb['TIPOLOGIA'].drop_duplicates()
+    # Convert the unique values to JSON format
     return df_tipologie.to_json()
 
-if __name__ == "__main__":
-    # Start server FastAPI
-    uvicorn.run(app, host="127.0.0.1", port=8081)
 
-#/find_structures_suburb?Typology=ALBERGO&English=Vero&French=Vero&German=Vero&Spanish=Vero
+if __name__ == "__main__":
+    # Start the FastAPI server using uvicorn on host 127.0.0.1 and port 8081
+    uvicorn.run(app, host="127.0.0.1", port=8081)
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,49 +1,175 @@
 import os
 import sys
 from fastapi.testclient import TestClient
+import unittest
+import pandas as pd
 
-# Add the project root to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Now you can do the relative import
-from app.main import app
+# Add the parent directory to the sys.path
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
 
+from app.main import app, df_suburb
+from app.main import find_structures_suburb
+import pytest
 
-"""
-Execute this test by running on the terminal (from the app/) the command:
-pytest --cov=app --cov-report=html tests/
- """
-
+# Create a TestClient instance for testing API endpoints
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_suburb_albergo_lang():
+    # Test Albergo all languages
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'ALBERGO',
+        'English': 'True',
+        'French': 'True',
+        'German': 'True',
+        'Spanish': 'True'
+    })
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    # Check if the reponse is a list
+    assert isinstance(response.json(), list)
 
 
-def test_success_read_item():
-    response = client.get("/query/Albert Einstein")
+def test_suburb_albergo_nolang():
+    # Test albergo no language
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'ALBERGO',
+        'English': 'Falso',
+        'French': 'Falso',
+        'German': 'Falso',
+        'Spanish': 'Falso'
+    })
+    # Check if the reponse is a list
+    assert isinstance(response.json(), list)
+
+
+def test_suburb_bed_and_breakfast():
+    # Test Bed and breakfast all languages
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'BED AND BREAKFAST',
+        'English': 'Vero',
+        'French': 'Vero',
+        'German': 'Vero',
+        'Spanish': 'Vero'
+    })
+    # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-    assert response.json() == {"person_name": 'Albert Einstein', 
-                               "birthday": '03/14/1879'}
+    # Check if the response content is a list
+    assert isinstance(response.json(), list)
+    # Check if the response list has at least one item
+    assert len(response.json()) > 0
 
 
-""" def test_fail_read_item():
-    response = client.get("/query/Pippo")
+def test_suburb_residence():
+    # Test Residence all languages
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'RESIDENCE',
+        'English': 'Vero',
+        'French': 'Vero',
+        'German': 'Vero',
+        'Spanish': 'Vero'
+    })
+    # Check if the response content is a list
+    assert isinstance(response.json(), list)
+    # Check if the response list has at least one item
+    assert len(response.json()) > 0
+
+
+def test_suburb_invalid():
+    # Test invalide typology
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'COUNTRYHOUSE',
+        'English': 'Vero',
+        'French': 'Vero',
+        'German': 'Vero',
+        'Spanish': 'Vero'
+    })
+    # Check if the response content is a list
+    assert isinstance(response.json(), list)
+    # Check if the response list is empty
+    assert len(response.json()) == 0
+
+
+def test_suburbs_hotel():
+    # Test invalid typology
+    response = client.get("/find_structures_suburb", params={
+        'Typology': 'HOTEL',
+        'English': 'Vero',
+        'French': 'Vero',
+        'German': 'Vero',
+        'Spanish': 'Vero'
+    })
+    # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-    assert response.json() == {"error": "Person not found"} """
+    # Check if the response content is an empty list
+    assert response.json() == []
 
 
-# The following will generate an error in pycheck
-""" def test_success_read_item_module():
-    response = client.get("/module/search/Albert Einstein")
-    assert response.status_code == 200
-    assert response.json() == {"Albert Einstein's birthday is 03/14/1879."} """
+def test_suburb_missing_values():
+    # Test to ensure no missing values
+    assert not df_suburb.isna().any().any()
 
 
-# The following is correct, can you spot the diffence?
-def test_success_read_item_module():
-    response = client.get("/module/search/Albert Einstein")
-    assert response.status_code == 200
-    assert response.json() == ["Albert Einstein's birthday is 03/14/1879."]
+class TestSearchStructures(unittest.TestCase):
+
+    def test_casa_vacanze(self):
+        # Test casa vacanze no German
+        # Call the find_structures_suburb function with specific parameters
+        result = find_structures_suburb(
+            Typology='CASA PER VACANZE',
+            English='Vero',
+            French='Vero',
+            German='Falso',
+            Spanish='Vero'
+        )
+        # Check if the result is a list
+        self.assertIsInstance(result, list)
+
+    def test_ostello(self):
+        # Test ostello only English
+        result = find_structures_suburb(
+            Typology='OSTELLO',
+            English='Vero',
+            French='Falso',
+            German='Falso',
+            Spanish='Falso'
+        )
+        self.assertIsInstance(result, list)
+
+    def test_bed_and_breakfast(self):
+        # Test bed and breakfast only Spanish
+        result = find_structures_suburb(
+            Typology='BED AND BREAKFAST',
+            English='Falso',
+            French='Falso',
+            German='Falso',
+            Spanish='Vero'
+        )
+        self.assertIsInstance(result, list)
+
+    def test_campeggio(self):
+        # Test campeggio no language
+        result = find_structures_suburb(
+            Typology='CAMPEGGIO',
+            English=False,
+            French=False,
+            German=False,
+            Spanish=False
+        )
+        self.assertIsInstance(result, list)
+        # Check if the result list is empty
+        self.assertEqual(len(result), 0)
+
+    def test_casa_religiosa(self):
+        # Test casa religiosa di ospitalità no language
+        result = find_structures_suburb(
+            Typology="CASA RELIGIOSA DI OSPITALITA' ",
+            English=False,
+            French=False,
+            German=False,
+            Spanish=False
+        )
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 0)
