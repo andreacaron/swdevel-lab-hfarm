b2beb8a44098109d7f4a12cb77c8105da137e665
ksofiadiazmontiel <888716@stud.unive.it>
2023-12-17 01:49:09 +0100

    Code revision 2

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -2,6 +2,9 @@ import os
 import sys
 import pytest
 from fastapi.testclient import TestClient
+
+# Add the project root to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.main import app, essential_services_periphery
 
 # Skipping performance tests
