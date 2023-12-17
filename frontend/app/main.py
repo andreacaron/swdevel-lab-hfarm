c32cc0030d2d35aee127a80fee09bb5e85746696
IlhameSebbar <150250385+IlhameSebbar@users.noreply.github.com>
2023-12-17 03:56:08 +0100

    Frontend Update

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,53 +1,78 @@
 # Importing the necessary libraries
-import requests  
+import requests
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
-from wtforms import StringField, SubmitField
 import json
 from flask import Flask, render_template
-import requests
 
 # Creating an instance of the Flask class
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  
+app.config['SECRET_KEY'] = 'your_secret_key'
+
 
 # Defining a FlaskForm for the search input fields
-class SearchForm(FlaskForm):
-   zona = SelectField('Zone:')
-   ristorante = SelectField('Restaurant:', choices=[('Vero', 'Yes'), ('Falso', 'No')])
-   parcheggio = SelectField('Parking', choices=[('Vero', 'Yes'), ('Falso', 'NO')])
-   submit = SubmitField('Search')
+class SearchEssentials(FlaskForm):
+    zona = SelectField('Zone:')
+    ristorante = SelectField('Restaurant:', choices=[
+        ('Vero', 'Yes'),
+        ('Falso', 'No')
+    ])
+    parcheggio = SelectField('Parking', choices=[
+        ('Vero', 'Yes'),
+        ('Falso', 'No')
+    ])
+    submit = SubmitField('Search')
+
 
 # Configuration for the FastAPI backend URL
-FASTAPI_BACKEND_HOST = 'http://backend'  
+FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
+
 
 # Defining a route for the internal page
 @app.route('/internal_page', methods=['GET', 'POST'])
 def find():
-   form = SearchForm()
-   error_message = None
-   response = requests.get(f'{FASTAPI_BACKEND_HOST}/get_zones')
-   aux = json.loads(response.json())
-   form.zona.choices = list(aux.values())
+    form = SearchEssentials()
+    error_message = None
+    response = requests.get(f'{FASTAPI_BACKEND_HOST}/get_zones')
+    aux = json.loads(response.json())
+    form.zona.choices = list(aux.values())
 
-   if form.validate_on_submit():
-       zona = form.zona.data
-       ristorante = form.ristorante.data
-       parcheggio = form.parcheggio.data
-       fastapi_url = f'{FASTAPI_BACKEND_HOST}/structures?zona={zona}&ristorante={ristorante}&parcheggio={parcheggio}'
-       try:
-           response = requests.get(fastapi_url)
-           response.raise_for_status()  # Raise an HTTPError for bad responses
-           data_from_fastapi, error_message = response.json(), None
-       except requests.exceptions.RequestException as e:
-           data_from_fastapi, error_message = None, f'Error: {str(e)}'
+    if form.validate_on_submit():
+        zona = form.zona.data
+        ristorante = form.ristorante.data
+        parcheggio = form.parcheggio.data
+        fastapi_url = (
+            f'{FASTAPI_BACKEND_HOST}/structures?'
+            f'zona={zona}&ristorante={ristorante}&parcheggio={parcheggio}'
+        )
+        try:
+            response = requests.get(fastapi_url)
+            response.raise_for_status()
+            data_from_fastapi, error_message = response.json(), None
+        except requests.exceptions.RequestException as e:
+            data_from_fastapi, error_message = None, f'Error: {str(e)}'
+
+        return render_template(
+            "internal_page.html",
+            form=form,
+            result=data_from_fastapi,
+            error_message=error_message
+        )
+
+    return render_template(
+        'internal_page.html',
+        form=form,
+        result=None,
+        error_message=error_message
+    )
 
 
-       return render_template("internal_page.html", form=form, result=data_from_fastapi, error_message=error_message)
+# Define a route for the explanatory homepage
+@app.route('/home_page')
+def home():
+    return render_template('home_page.html')
 
-   return render_template('internal_page.html', form=form, result=None, error_message=error_message)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80, debug=True)
-
